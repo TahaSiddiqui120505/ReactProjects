8c54a9a0feaa2cf0b09cabe27fcca0786fc809c2
Taha Siddiqui <siddiquitaha120@gmail.com>
2024-12-19 21:48:56 +0530

    5th Project Done

--- basic-projects/src/App.jsx
+++ basic-projects/src/App.jsx
@@ -3,6 +3,8 @@ import './App.css'
 import RandomColor from './components/2.RandomColor'
 import Accordian from './components/1.Accordian'
 import StarRating from './components/3.star-rating'
+import ImageSlider from './components/4.image-slider'
+import LoadMoreData from './components/5.load-more-data'
 
 function App() {
 
@@ -11,6 +13,8 @@ function App() {
       <Accordian></Accordian>
       <RandomColor></RandomColor>
       <StarRating noOfStars={10}></StarRating>
+      <ImageSlider url={"https://picsum.photos/v2/list"} page={"1"} limit={"10"}></ImageSlider>
+      <LoadMoreData></LoadMoreData>
       </div>
   )
 }
